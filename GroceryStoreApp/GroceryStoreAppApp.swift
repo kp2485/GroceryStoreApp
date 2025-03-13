8780249e5e6f48480dde19e285788d551e2fb246
Kyle Peterson <kdpeters@gmail.com>
2025-03-13 11:22:35 -0400

    Initial Commit

---
+++ GroceryStoreApp/GroceryStoreAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryStoreAppApp.swift
+//  GroceryStoreApp
+//
+//  Created by Kyle Peterson on 3/13/25.
+//
+
+import SwiftUI
+
+@main
+struct GroceryStoreAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GroceryStoreAppTests/GroceryStoreAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryStoreAppTests.swift
+//  GroceryStoreAppTests
+//
+//  Created by Kyle Peterson on 3/13/25.
+//
+
+import Testing
+@testable import GroceryStoreApp
+
+struct GroceryStoreAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
