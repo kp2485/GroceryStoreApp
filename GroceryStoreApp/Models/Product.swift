3d6757fdbceeeb7f76ecf5d28ccafd6bd68f0cd0
Kyle Peterson <kdpeters@gmail.com>
2025-03-13 11:29:57 -0400

    defined Product

---
+++ GroceryStoreApp/Models/Product.swift
@@ -0,0 +1,20 @@
+//
+//  Product.swift
+//  GroceryStoreApp
+//
+//  Created by Kyle Peterson on 3/13/25.
+//
+
+import Foundation
+
+struct Product: Identifiable, Hashable {
+    let id = UUID()
+    let name: String
+    let imageName: String
+    let brand: String
+    let price: Double
+    let salePrice: Double?
+    let category: ProductCategory
+    let inStock: Bool
+    let onSale: Bool
+}
